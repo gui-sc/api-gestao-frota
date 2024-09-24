d2e6d25a3aa6f3faeb309cd46e184034ef001ced
Guilherme Coelho <guilhermeifc7@gmail.com>
2024-09-24 19:38:58 -0300

    Revert "add profile screen"
    This reverts commit 7a5608b2854e1c75ff4b4c8ede47efcdc47d9297.

--- utils/stackParamRouteList.ts
+++
@@ -1,8 +0,0 @@
-import { StackNavigationProp } from "@react-navigation/stack";
-
-export type RootStackParamList = {
-    login: undefined;
-    index: undefined;
-};
-
-export type RouteList = StackNavigationProp<RootStackParamList>
