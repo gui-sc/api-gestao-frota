d4a62fc393014b85902ce57043ecad92697a369a
guilherme coelho <guilhermeifc7@gmail.com>
2024-10-10 20:10:21 -0300

    fix: add functions to router

--- src/routes/TravelRouter.ts
+++ src/routes/TravelRouter.ts
@@ -3,8 +3,12 @@ import * as Controller from '../controllers/TravelController';
 const router = Router();
 
 router.post('/', Controller.create)
-router.get('/', Controller.getByRange)
 router.post('/:id', Controller.acceptTravel)
+router.put('/:id/init', Controller.initTravel)
+router.put('/:id/finish', Controller.finishTravel)
+router.get('/', Controller.getByRange)
+router.get('/last/passenger/:id', Controller.getLastTravelsPassenger)
+router.get('/last/driver/:id', Controller.getLastTravelsDriver)
 router.delete('/:id', Controller.remove)
 
 export default router;
