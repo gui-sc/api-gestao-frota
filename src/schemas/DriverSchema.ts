bac6b8d4d8b4ffe55b5bedf698300087dc99ec0a
Alysson <alyssonempire84@gmail.com>
2024-10-17 10:31:52 -0300

    Aprimorada a validação da data de nascimento e da CNH

--- src/schemas/DriverSchema.ts
+++ src/schemas/DriverSchema.ts
@@ -1,19 +1,25 @@
 import { z } from 'zod';
 
 export const DriverSchema = z.object({
-    nome: z.string({ message: "Você precisa passar o nome" }),
+    nome: z.string(),
     cpf: z.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).transform((cpf) => {
         //Remove os caracteres especiais do CPF
         return cpf.replace(/[^\d]/g, "");
     }),
 
-    data_nasc: z.string().regex(/^\d{2}\/\d{2}\/\d{4}$/).transform((dateString) => {
+    data_nasc: z.string().regex(/^\d{2}\/\d{2}\/\d{4}$/, { message: 'Data invalida' }).transform((dateString) => {
         //Converte a string em um objeto Date
         const [day, month, year] = dateString.split("/");
         return new Date(`${year}-${month}-${day}`);
-    }),
+    }).refine((date) => {
+        //Verifica se é um viajante do tempo
+        if (date < new Date()) {
+            return true;
+        }
+        return false;
+    }, { message: 'Data de nascimento nao pode ser no futuro e nem atual' }),
 
-    cnh: z.string().length(11),
+    cnh: z.string().length(11, { message: 'CNH deve ter 11 caracteres' }),
     logradouro: z.string(),
     bairro: z.string(),
     cidade: z.string(),
