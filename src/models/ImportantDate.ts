9278fafb4517270d7b4f5ad9ca2ac7281d07e8f9
Alysson <alyssonempire84@gmail.com>
2024-10-30 10:49:42 -0300

    Separando ImportantDateModel

---
+++ src/models/ImportantDate.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database";
+
+export const ImportantDateModel = sequelize.define('important_date', {
+    driver_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'drivers',
+            key: 'id'
+        }
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
