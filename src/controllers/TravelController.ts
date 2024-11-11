c3872703e92d12d0e8195da970f63b34f5927ff6
guilherme coelho <guilhermeifc7@gmail.com>
2024-11-11 20:26:09 -0300

    feat: incluir nome e avatar no agrupamento de conversas e retornar ID da viagem ao criar

--- src/controllers/TravelController.ts
+++ src/controllers/TravelController.ts
@@ -120,11 +120,12 @@ export async function getByRange(req: Request, res: Response) {
 
 export async function create(req: Request, res: Response) {
     try {
-        await TravelModel.create({
+        const travel = await TravelModel.create({
             ...req.body
-        });
-        res.status(200).json({ message: 'Viagem solicitada com sucesso!' });
+        }) as any;
+        res.status(200).json({ message: 'Viagem solicitada com sucesso!', id: travel.id });
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: 'Erro ao solicitar viagem!' });
     }
 }
