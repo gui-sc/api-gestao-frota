55453225ed38016efa0ad05e8fc78d1415a24acd
Alysson <alyssonempire84@gmail.com>
2024-11-12 15:46:38 -0300

    feat: implementar criação de mensagens de reprovação de motoristas

--- src/controllers/DriverDeclineMessageController.ts
+++ src/controllers/DriverDeclineMessageController.ts
@@ -3,7 +3,18 @@ import { DriverDeclineMessageModel } from "../models/DriverDeclineMessage";
 import sequelize from "../database";
 
 export async function create(req: Request, res: Response) {
-
+    const { driver_id, message, user_id } = req.body;
+    try {
+        const driverDeclineMessage = await DriverDeclineMessageModel.create({
+            driver_id: driver_id,
+            message: message,
+            user_id: user_id,
+            read: undefined
+        });
+        return res.status(201).json(driverDeclineMessage);
+    } catch (error) {
+        return res.status(500).json({ message: "Erro ao criar mensagem!", error });
+    }
 }
 
 export async function get(req: Request, res: Response) {
