8ceced77846c07889a590738ab4fddb174e5d97b
Alysson <alyssonempire84@gmail.com>
2024-11-13 16:46:01 -0300

    desfazendo a gambiarra

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -140,15 +140,12 @@ export async function loginApp(req: Request, res: Response) {
 
 export async function loginAdmin(req: Request, res: Response) {
     try {
-        const { email, password, time } = req.body;
+        const { email, password } = req.body;
         const user = await UserModel.findOne({ where: { email } }) as any;
         if (!user) return res.status(401).json({ message: "Credenciais incorretas" });
         if (!bcrypt.compareSync(password, user.password)) return res.status(401).json({ message: "Credenciais incorretas" });
         if (user.type !== 'admin') return res.status(401).json({ message: "Credenciais incorretas" });
-
-        //acresente 30 minutos em milissegundos
-        const sessionTime = time + 30 * 60 * 1000;
-        res.status(200).json({ sessionTime, email });
+        res.status(200).json({ email });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Erro ao fazer login", err });
