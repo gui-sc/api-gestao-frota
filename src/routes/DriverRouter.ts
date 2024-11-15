52e98828b142cb4bfc3e5765d54fb2ada7946c56
guilherme coelho <guilhermeifc7@gmail.com>
2024-11-15 15:19:12 -0300

    feat: adicionar suporte para upload e exclusão de arquivos de motorista e veículo, incluindo validação de arquivos obrigatórios

--- src/routes/DriverRouter.ts
+++ src/routes/DriverRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import * as Controller from '../controllers/DriverController'; 
+import * as Controller from '../controllers/DriverController';
 import multer from 'multer';
 import multerConfig from '../config/MulterConfig';
 const router = Router();
 
-router.post('/',multer(multerConfig).fields(
+router.post('/', multer(multerConfig).fields(
     [
         { name: 'profile_picture', maxCount: 1 },
         { name: 'cnh_picture', maxCount: 1 },
@@ -15,7 +15,13 @@ router.get('/', Controller.get);
 router.get('/pending', Controller.getPending);
 router.get('/:id', Controller.getById);
 router.put('/:id/approved', Controller.aproveDriver);
-router.put('/:id', Controller.update);
+router.put('/:id', multer(multerConfig).fields(
+    [
+        { name: 'profile_picture', maxCount: 1 },
+        { name: 'cnh_picture', maxCount: 1 },
+        { name: 'profile_doc_picture', maxCount: 1 }
+    ]
+), Controller.update);
 router.delete('/:id', Controller.remove);
 
 export default router;
