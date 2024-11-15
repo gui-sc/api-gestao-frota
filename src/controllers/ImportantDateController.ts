d407543766f337c61f9787e93266537e1cc7fd8e
Alyssonzin <alyssonempire84@gmail.com>
2024-11-15 14:30:15 -0300

    Tratando range na busca de datas importantes

--- src/controllers/ImportantDateController.ts
+++ src/controllers/ImportantDateController.ts
@@ -4,13 +4,13 @@ import { ImportantDateModel } from "../models/ImportantDate";
 
 export async function create(req: Request, res: Response) {
     try {
-        const { driver, date, description } = req.body
+        const { driver_id, date, description } = req.body
+        const [day, month, year] = date.split("/");
+        const iDate = new Date(Number(year), Number(month) - 1, Number(day));
 
-        const iDate = new Date(date);
+        const importantDate = await ImportantDateModel.create({ driver_id, date: iDate, description })
 
-        await ImportantDateModel.create({ driver_id: driver, date: iDate, description })
-
-        return res.status(201).json({ message: "Data importante criada" })
+        return res.status(201).json({ message: "Data importante criada", importantDate })
     } catch (error) {
         console.error("Error:", error)
         res.status(500).json({ message: "Erro ao criar data importante" })
@@ -23,7 +23,7 @@ export async function getAllDates(req: Request, res: Response) {
 
         const dates = await ImportantDateModel.findAll({
             where: {
-                driver: Number(id)
+                driver_id: Number(id)
             }
         });
 
@@ -38,13 +38,27 @@ export async function getNextDates(req: Request, res: Response) {
     try {
         const { id } = req.params;
 
-        const today = new Date();
-        const endDate = new Date();
+        const today = new Date(
+            Date.UTC(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                new Date().getDate()
+            )
+        );
+
+        const endDate = new Date(
+            Date.UTC(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                new Date().getDate()
+            )
+        );
+
         endDate.setDate(today.getDate() + 14); // Calcula a data de 14 dias a partir de hoje
 
         const dates = await ImportantDateModel.findAll({
             where: {
-                driver: Number(id),
+                driver_id: Number(id),
                 date: {
                     [Op.between]: [today, endDate] // Filtra datas entre hoje e 14 dias a partir de hoje
                 }
@@ -53,8 +67,7 @@ export async function getNextDates(req: Request, res: Response) {
 
         return res.status(200).json(dates);
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ message: "Erro ao buscar datas importantes" });
+        res.status(500).json({ message: "Erro ao buscar datas importantes", error });
     }
 }
 
@@ -62,7 +75,7 @@ export async function deleteDate(req: Request, res: Response) {
     try {
         const { id } = req.params;
 
-        await ImportantDateModel.destroy({ where: { id: Number(id) } }).then(res => {console.log("teste") })
+        await ImportantDateModel.destroy({ where: { id: Number(id) } }).then(res => { console.log("teste") })
         return res.status(204).send();
     } catch (error) {
         console.error("Error:", error);
